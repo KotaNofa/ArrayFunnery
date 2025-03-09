9c2260085da00300332e3c779791f6ff5ba60d45
Ari <79033264+KotaNofa@users.noreply.github.com>
2025-03-08 23:16:40 -0800

    new circle drawer with redundant degree select
    features: uses vectors and heap memory.
    crashes when set to 0 lol

--- ArrayFunnery/main.cpp
+++ ArrayFunnery/main.cpp
@@ -1,72 +1,99 @@
 #define _USE_MATH_DEFINES
-#include <iostream>
-#include <cmath>
+
 #include <vector>
+#include <cmath>
+#include <iostream>
 
 using namespace std;
 
-const int rows = 240;
-const int cols = 240;
+struct Point
+{
+	string l;
+	double vX;
+	double vY;
+	double r;
+	int iX;
+	int iY;
+};
 
-int degrees = 0;
-int iterations = 0;
+Point start;
+int width;
+int height;
+vector<vector<Point*>>screen;
+int iterations;
 
-string screen1[rows][cols];
+void Init() {
 
-pair<int, int> point(80, 80);
-
-int originrow(int index) {
-
-	return (rows / 2) - index;
-
-}
-
-int origincol(int index) {
-
-	return (cols / 2) + index;
-}
-
-void rotate(pair<int, int>& coords) {
-    double angle = degrees * (M_PI / 180.0);  // Rotation by 1 degree
-    int x = coords.second;
-    int y = coords.first;
-    coords.second = static_cast<int>(x * cos(angle) - y * sin(angle));
-    coords.first = static_cast<int>(x * sin(angle) + y * cos(angle));
-}
+	cout << "Enter X from origin" << endl;
+	cin >> start.vX;
+	cout << "Enter Y from origin" << endl;
+	cin >> start.vY;
+	cout << "Enter rotation amount in degrees" << endl;
+	cin >> start.r;
 
 
-int main() {
+	width = 3 * start.vX;
+	height = 3 * start.vY;
 
-	for (int i = 0; i < rows; ++i) {
-		for (int j = 0; j < cols; ++j) {
-			screen1[i][j] = " .";
+	if (width < height) {
+		width = height;
+	}
+	else {
+		height = width;
+	}
+
+	screen.resize(width);
+	for (int i = 0; i < width; ++i) {
+		screen[i].resize(height);
+
+		for (int j = 0; j < height; ++j) {
+
+			Point* fill = new Point;
+			fill->l = ".";
+
+			screen[i][j] = fill;
 		}
 	}
 
-	screen1[origincol(0)][originrow(0)] = " o";
+	Point* origin = new Point;
+	origin->l = "0";
+	screen[width / 2][height / 2] = origin;
 
-	cout << "start frame, hold enter" << endl;
-	cout << "select rotation amount per iteration" << endl;
-	cin >> degrees;
+	iterations = M_PI * 2 * height;
+}
 
-	while (cin.get()) {
-		if (cin.get() == 'k') {
-			break;
-		}
-		rotate(point);
-		screen1[originrow(point.first)][origincol(point.second)] = " 0";
-		cout << "point x" << point.second << endl;
-		cout << "point y" << point.first << endl;
-		cout << "iteration: " << iterations << ". submit 'ok' to display" << endl;
-		++iterations;
+void computeRotation(Point start, double degrees) {
+	double radians = degrees * (M_PI / 180);
+
+	for (int i = 0; i < iterations; ++i) {
+		Point* update = new Point;
+		update->l = "0";
+
+		double angle = (i * 2 * M_PI) / iterations;
+
+		update->vX = start.vX * cos(angle) - start.vY * sin(angle);
+		update->vY = start.vX * sin(angle) + start.vY * cos(angle);
+
+		update->iX = (height / 2) - update->vY;
+		update->iY = (width / 2) + update->vX;
+		screen[update->iX][update->iY] = update;
 	}
+}
 
-	for (int i = 0; i < rows; ++i) {
-		for (int j = 0; j < cols; ++j) {
-			cout << screen1[i][j];
+void printScreen() {
+	for (int i = 0; i < width; ++i) {
+		for (int j = 0; j < height; ++j) {
+			cout << screen[i][j]->l << " ";
 		}
 		cout << endl;
 	}
+}
+
+int main() {
+
+	Init();
+	computeRotation(start, start.r);
+	printScreen();
 
 	return 0;
 }
